6951fb41e1982d16b1c474abd648a149c9ec56ab
Kenneth <geriskenneth@gmail.com>
2022-05-05 15:31:53 -0400

    Update test cases and console.logs

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ import { Blockchain } from './blockchain.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.time(`blockchain`);
 const chain = new Blockchain();
 const a = new Block("first block");
 const b = new Block("second block");
 const c = new Block("third block");
 
-console.log("Mining block 1...");
+console.log(`Mining block 1...`);
 chain.addNewBlock(a);
 
-console.log("Mining block 2...");
+console.log(`Mining block 2...`);
 chain.addNewBlock(b);
 
-console.log("Mining block 3...");
+console.log(`Mining block 3...`);
 chain.addNewBlock(c);
 
 // Tamper with the chain
-chain.blockchain[1].data = "tampered block";
+//chain.blockchain[1].data = "tampered block";
 
 // Blockchain validity
-console.log('Blockchain valid? ' + chain.validateBlockChain()); // will return false!
+console.log(`Blockchain valid? ${chain.validateBlockChain()}`); // will return false!
+console.timeEnd(`blockchain`);
 
 console.log(JSON.stringify(chain, null, 4));
